9c34801f5265b64d8538754d8775ba8a8b69794a
Glitch (imported-pluto) <none>
2019-10-02 07:47:13 +0000

    🐂📊 Checkpoint ./server.js:7037687/205

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res){
+  var header = req.params.;
+  console.log(header);
+  res.json({"ipaddress": "", "language": "", "software": ""});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
